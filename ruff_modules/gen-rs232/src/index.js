539de750e18d22bd064b1ee81c9f80f10119b440
yango3 <o3.yang@hotmail.com>
2018-04-17 16:13:26 +0800

    normal update

--- ruff_modules/gen-rs232/src/index.js
+++ ruff_modules/gen-rs232/src/index.js
@@ -6,18 +6,20 @@ module.exports = driver({
 
     attach: function (inputs, context) {
         // this._<interface> = inputs['<interface>'];
-	this._uart = inputs['uart'];
+        this._uart = inputs['uart'];
 
-	this._uart.on('data', function(data){
+        var that = this;
 
-	    this.emit('data',data);
-	});
+        this._uart.on('data', function (data) {
+
+            that.emit('data', data);
+        });
     },
 
     exports: {
         write: function (buf, cb) {
             // this._<interface>.<method>
-	    this._uart.write(buf,cb);
+            this._uart.write(buf, cb);
         }
     }
 
